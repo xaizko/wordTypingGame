7bdbdbf0027ad12ea0dec6cf96cf8a45ea7c2d1f
xaizko <tonytran177013@gmail.com>
2025-06-02 18:06:08 -0400

    Updated declarations

--- typingGame.h
+++ typingGame.h
@@ -11,31 +11,7 @@
 //Api libraries
 #include <curl/curl.h>
 
-//code taken from https://github.com/xaizko/UserInputTimer
-//This function simply acts as a input timer
-char *timeOut(int timeout)  {
-    static char buf[512];
-    fd_set rfds;
-    struct timeval tv;
-    int retval;
-
-    FD_ZERO(&rfds);
-    FD_SET(0, &rfds);
-
-    tv.tv_sec = timeout;
-    tv.tv_usec = 0;
-
-    retval = select(1, &rfds, 0, 0, &tv);
-    if (retval && FD_ISSET(0, &rfds)) {
-	memset(buf, 0, 512);
-	retval = read(0, buf, 511);
-	if (retval < 1) {
-	    return 0;
-	}
-	retval--;
-	buf[retval] = 0;
-	return buf;
-    } else {
-	return 0;
-    }
-}
+//Function headers
+char *timeOut(int timeout); //Function to give user input a timer
+size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp); //Function to write response to a string
+void sampleCurlCall(CURL *curl, CURLcode res, char *response); //Test api call function
